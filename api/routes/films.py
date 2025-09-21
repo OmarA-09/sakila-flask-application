064479b5abea5ba35af67a10528380ed6f692007
Omar Ahmad <omarahmad6223@gmail.com>
2025-09-21 23:23:28 +0100

    Add PUT, PATCH for film

--- api/routes/films.py
+++ api/routes/films.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from marshmallow import ValidationError
+from sqlalchemy.exc import IntegrityError
 
 from api.models import db
 from api.models.film import Film
@@ -34,13 +35,62 @@ def create_film():
     db.session.commit()
 
     return film_schema.dump(film), 201
-'''
 
+@films_router.delete('/<film_id>')
+def delete_film(film_id):
+    film = Film.query.get(film_id)
+    if film is None:
+        return {"error": "Film not found"}, 404
 
-    actor = Actor(**actor_data)         # Create a new actor model
-    db.session.add(actor)               # Insert the record
-    db.session.commit()                 # Update the database
+    try:
+        db.session.delete(film)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return {
+            "error": f"Film {film_id} cannot be deleted because it is referenced in other records."
+        }, 409
+    
+    return film_schema.dump(film), 204
 
-    return actor_schema.dump(actor), 201     # Serialize the created actor
+@films_router.put("/<film_id>")
+def update_film_full_name(film_id):
+    film = Film.query.get(film_id)
+    if film is None:
+        return {"error": "Film not found"}, 404
 
-'''
+    film_data = request.json
+    try:
+        # validate incoming JSON
+        film_schema.load(film_data, partial=False)  # full object required
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    # overwrite fields
+    for key, value in film_data.items():
+        if hasattr(film, key):
+            setattr(film, key, value)
+
+    db.session.commit()
+    return film_schema.dump(film), 200
+
+@films_router.patch("/<film_id>")
+def partial_update_film(film_id):
+    film = Film.query.get(film_id)
+    if film is None:
+        return {"error": "Film not found"}, 404
+
+    film_data = request.json
+    try:
+        # only provided fields validated
+        film_schema.load(film_data, partial=True)
+    except ValidationError as err:
+        return {"error": err.messages}, 400
+
+    # update only given fields
+    for key, value in film_data.items():
+        if hasattr(film, key):
+            setattr(film, key, value)
+
+    db.session.commit()
+    return film_schema.dump(film), 200
