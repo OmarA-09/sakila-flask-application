64185294dca8ad847222656c24c2beeed44900fc
Omar Ahmad <omarahmad6223@gmail.com>
2025-10-07 14:52:03 +0100

    Remove secrets from config file

---
+++ api/config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config(object):
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+
+match os.getenv('ENV'):
+    case 'PRODUCTION':
+        config = ProdConfig
+    case _:
+        config = DevConfig
